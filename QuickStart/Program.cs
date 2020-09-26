7b69901f7bb5ee982e365a77680af12def66f64e
Alexander Torrijos <artorrijos@gmail.com>
2020-09-26 20:36:51 +0800

    Initial Commit

---
+++ QuickStart/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+
+            var log = log4net.LogManager.GetLogger(typeof(Program));
+
+            log.Info("Hello from log4net");
+
+            Console.ReadLine();
+
+        }
+    }
+}
